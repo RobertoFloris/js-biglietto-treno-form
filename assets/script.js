ef1e88cd81eb89a2b4eb96a16bba129a6acf5456
RobertoFloris <florisroberto99@gmail.com>
2024-11-26 09:57:38 +0100

    [fix] layout using Bootstrap

--- assets/script.js
+++ assets/script.js
@@ -5,43 +5,45 @@ const km = document.getElementById("km");
 const age = document.getElementById("age");
 const button = document.getElementById("btn");
 
-const card = document.querySelector(".card")
+const card = document.querySelector(".carta")
 
 const nameCard = document.getElementById("name-card")
 const kmCard = document.getElementById("km-card");
 const ageCard = document.getElementById("age-card");
 const priceCard = document.getElementById("price-card");
+const carrozCard = document.getElementById("carroz-card");
+const cpCard = document.getElementById("cp-card");
 
 
-form.addEventListener("submit", () => {
-  card.classList.toggle("none");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-  if(!(card.classList.contains("none"))){
-    button.innerText = "Annulla";
-  } else {
-    button.innerText = "Genera";
-    name.value = "";
-    km.value = "";
-    age.value = "";
-  }
+  card.classList.remove("d-none");
   
   nameCard.innerText = "Benvenuto a bordo, " + name.value.trim() + "!";
   kmCard.innerText = km.value.trim() + "km";
   ageCard.innerText = age.value.trim() + " anni";
+  carrozCard.innerText = "Carrozza: " + Math.floor(Math.random() * 6) + 1;
+  cpCard.innerText = "CP: 0" + Math.floor(Math.random() * 9999);
+
   
   let price = km.value * 0.21;
 
-  let message = " Il prezzo del tuo biglietto è: "+ price.toFixed(2) + "€.";
+  let message = " Intero Web: "+ price.toFixed(2) + "€.";
 
   if (age.value<18){
     price*=0.8; //20% di sconto
-    message += " Sei minorenne per cui ti viene applicato uno sconto del 20%. Il prezzo aggiornato è: "+ price.toFixed(2) + "€";
+    message += " Sconto Under18: "+ price.toFixed(2) + "€";
   } else if(age.value>=65) {
    price*=0.6; //40% di sconto
-   message += " Sei over65 per cui ti viene applicato uno sconto del 40%. Il prezzo aggiornato è: "+ price.toFixed(2) + "€";
+   message += " Sconto over65: "+ price.toFixed(2) + "€";
   }
 
   priceCard.innerText = message;
+})
+
+form.addEventListener("reset", () =>{
+  card.classList.add("d-none")
 })
 
 
